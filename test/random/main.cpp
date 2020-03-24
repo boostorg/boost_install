f93818c02d19f16a33364891d528ca3aeef06bba
Peter Dimov <pdimov@gmail.com>
2020-03-24 16:36:03 +0200

    Make test/random use an intermediate library that links to Boost::random privately

--- test/random/main.cpp
+++ test/random/main.cpp
@@ -1,19 +1,17 @@
 
-// Copyright 2017, 2018 Peter Dimov.
+// Copyright 2017-2020 Peter Dimov.
 //
 // Distributed under the Boost Software License, Version 1.0.
 //
 // See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/random/random_device.hpp>
 #include <boost/core/lightweight_test.hpp>
+
+unsigned get_random_number();
 
 int main()
 {
-    boost::random::random_device dev;
-
-    BOOST_TEST_NE( dev(), dev() );
-
+    BOOST_TEST_NE( get_random_number(), get_random_number() );
     return boost::report_errors();
 }
